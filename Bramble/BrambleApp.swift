1cd73efa039c6456595f5d27f231fbe438aa188e
generic-keith <keith.staines.ext@bbc.co.uk>
2025-02-15 04:49:42 +0000

    Initial Commit

---
+++ Bramble/BrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrambleApp.swift
+//  Bramble
+//
+//  Created by Keith Staines on 15/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct BrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
